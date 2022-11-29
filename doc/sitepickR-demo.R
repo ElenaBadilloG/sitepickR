17e47285a892b6ece72759cd2ac1f2a8bb6b27d8
Elena Badillo <elenabadillog@uchicago.edu>
2022-11-29 07:55:47 -0600

    Add files via upload

--- doc/sitepickR-demo.R
+++ doc/sitepickR-demo.R
@@ -1,70 +1,126 @@
 ## ---- include = FALSE---------------------------------------------------------
-options(rmarkdown.html_vignette.check_title = FALSE) 
-knitr::opts_chunk$set(warning = FALSE, message = FALSE, fig.width=7, fig.height=5) 
+options(rmarkdown.html_vignette.check_title = FALSE)
+knitr::opts_chunk$set(warning = FALSE, message = FALSE, fig.width=8, fig.height=5) 
 knitr::opts_chunk$set(
-  collapse = TRUE,
+  collapse = FALSE,
   comment = "#>"
 )
 
 ## -----------------------------------------------------------------------------
+if(!require(devtools)){
+    install.packages("devtools", repos = "http://cran.us.r-project.org")
+}
 
+if(!require(sitepickR)){
+    devtools::install_github("ElenaBadilloG/sitepickR")
+    }
+
+## -----------------------------------------------------------------------------
 library(sitepickR)
 
-## -----------------------------------------------------------------------------
-dfCCD <- sitepickR::dfCCD
-head(dfCCD)
+## ---- results='asis'----------------------------------------------------------
+rawCCD <- sitepickR::rawCCD
+knitr::kable(head(rawCCD), format = "html")
+
+## ---- results='asis'----------------------------------------------------------
+dfCCD <- prepDF(rawCCD,
+                 unitID="LEAID", subunitID="NCESSCH")
+knitr::kable(dfCCD[1:10,(ncol(dfCCD)-5-1):ncol(dfCCD)], format = "html")
 
 ## -----------------------------------------------------------------------------
-PATH = "SOME-USER-FOLDER/" 
-replDISTRICTS = paste0(PATH, "/replacementUnits.csv") # {initial district:[replacements]} directory
-dictSCHOOLS = paste0(PATH, "/subUnitTable.csv") # {district: [schools]} directory
+seed <- 1122 
 
 ## -----------------------------------------------------------------------------
-seed = 1234 
+calip <- 0.2 # maximum standard deviations of covariate distance around target population
 
 ## -----------------------------------------------------------------------------
-unitSamp_varsCCD <- c("w.pct.frlunch", "w.pct.black", "w.pct.hisp", "w.pct.female") 
+Nu <- 100 # district sample size
+K <- 10 # number of matches per district
+Ns <- 5 # number of schools per potential district
 
 ## -----------------------------------------------------------------------------
-subUnitSamp_varsCCD <- c("sch.pct.frlunch", "sch.pct.black", "sch.pct.hisp", "sch.pct.female")
+uSampVarsCCD <- c("w.pct.frlunch", "w.pct.black", "w.pct.hisp", "w.pct.female") 
 
 ## -----------------------------------------------------------------------------
-exact_match_vars <-  c("distr.type")
+suSampVarsCCD <- c("sch.pct.frlunch", "sch.pct.black", "sch.pct.hisp", "sch.pct.female")
 
 ## -----------------------------------------------------------------------------
-calip_match_vars <-  c("w.pct.black", "w.pct.hisp", "w.pct.female")
+exactMatchVars <-  c("distr.type")
 
 ## -----------------------------------------------------------------------------
+calipMatchVars <-  c("w.pct.black", "w.pct.hisp", "w.pct.female")
 
-m.out <- selectMatch(df=dfCCD, # dataset
-                       unit_ID="LEAID", # column name of district ID
-                       subUnit_ID="NCESSCH", # column name of school ID
-                       unit_vars=unitSamp_varsCCD,
-                       subUnit_samp_vars=subUnitSamp_varsCCD,
-                       exact_match_vars=NULL,
-                       calip_match_vars=calip_match_vars , 
-                       nUnitSamp = 100, # original district sample size
-                       nRepUnits = 10, # number of desired matches per selected district
-                       nsubUnits = 5, # number of schools to sample from each candidate district
-                       calipValue = 0.2, # 
-                       seedN = seed,
-                       matchDistance = "mahalanobis",
+## -----------------------------------------------------------------------------
+smOut <- selectMatch(df = dfCCD, # user dataset
+                       unitID = "LEAID", # column name of unit ID in user dataset
+                       subunitID = "NCESSCH", # column name of sub-unit ID in user dataset
+                       unitVars = uSampVarsCCD, # name of unit level covariate columns
+                       subunitSampVars = suSampVarsCCD, # name of sub-unit level covariate columns
+                       exactMatchVars = exactMatchVars, # unit level categorical covariates on which to match exactly
+                       calipMatchVars = calipMatchVars, # unit level numeric covariates on which to match within a radius
+                       nUnitSamp = Nu, # original unit sample size
+                       nRepUnits = K, # number of desired matches per initially selected unit
+                       nsubUnits = Ns, # number of sub-units to sample from each candidate unit
+                       calipValue = calip, # maximum distance on which to match specified unit level covariates (calipMatchVars)
+                       seedN = seed, # random seed number
+                       matchDistance = "mahalanobis", # metric used for matching units
                        sizeFlag = TRUE,
-                       replaceFlag = FALSE, #match without replacement
-                       writeOut = FALSE, # write out a csv file for: 1) matched units and 2) selected schools
-                       replacementUnitsFilename = replDISTRICTS,
-                       subUnitTableFilename = dictSCHOOLS
+                       repFlag = FALSE, # pick matches without repetition
+                       writeOut = FALSE, # write out a csv file for: 1) matched units and 2) selected sub-units
+                       replacementUnitsFilename = "replacementsTable.csv", # filename for {districtA: [districtA replacement list]} table
+                       subUnitTableFilename = "schoolsDirectory.csv" # filename for {districtA: [districtA schools]} table
 )
 
-## ----fig.width=5, fig.height=5------------------------------------------------
-getUnitLovePlot(m.out)
+## ---- results='asis'----------------------------------------------------------
+districtReplTable <- smOut[[1]]
+knitr::kable(head(districtReplTable), format = "html")
 
-## -----------------------------------------------------------------------------
-getUnitReplacementBalance(m.out)
+## ---- results='asis'----------------------------------------------------------
+schoolDirectory <- smOut[[2]]
+knitr::kable(head(dplyr::filter(schoolDirectory, !is.na(Sub_unit5_ID))), format = "html")
 
-## -----------------------------------------------------------------------------
-getMatchCount(m.out)
 
-## -----------------------------------------------------------------------------
-getSubUnitBalance(m.out)
+## ----fig.width=6, fig.height=5------------------------------------------------
+unitLovePlot(smOut,
+   title = "Standardized Mean Difference: \n Initially Selected Units vs. Population")
+
+## ---- results='asis'----------------------------------------------------------
+unitBalanceTab <- getSummary(smOut, diagnostic="unitBal")
+
+knitr::kable(head(unitBalanceTab, 10), format = "html")
+
+## ----fig.width=9, fig.height=6------------------------------------------------
+matchBalance(smOut, 
+  title = "Standardized Mean Difference: \
+  Replacement Unit Groups (1...K) vs. Originally Selected Units")
+
+## ---- results='asis'----------------------------------------------------------
+matchBalanceTab <- getSummary(smOut, diagnostic="matchBal")
+knitr::kable(head(matchBalanceTab, 20), format = "html")
+
+## ----fig.width=7, fig.height=6------------------------------------------------
+matchFreq(smOut,
+             title="Match Frequency per Original Unit")
+
+## ----fig.width=7, fig.height=6------------------------------------------------
+matchCount(smOut,
+             title="% of Successful Matches per Unit Group")
+
+## ---- results='asis'----------------------------------------------------------
+matchFreqTab <- getSummary(smOut, diagnostic="matchFreq")
+
+knitr::kable(matchFreqTab, format = "html")
+
+## ---- results='asis'----------------------------------------------------------
+matchCountTab <- getSummary(smOut, diagnostic="matchCount")
+
+knitr::kable(matchCountTab, format = "html")
+
+## ----fig.width=9, fig.height=6------------------------------------------------
+subUnitBalance(smOut,
+     title="Standardized Mean Difference: \n Sub-units from Original + Replacement Unit Groups vs. Population")
+
+## ---- results='asis'----------------------------------------------------------
+subUnitBalanceTab <- getSummary(smOut, diagnostic="subunitBal")
+knitr::kable(subUnitBalanceTab, format = "html")
 
